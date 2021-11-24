a6f84c6e4cbf4b13f244d276ecd8d963efe82780
Gerardo Camorlinga Jr <83178346+gercamjr@users.noreply.github.com>
2021-11-23 20:08:23 -0600

    fixed package.json and added desc ordering of blog

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fileupload = require('express-fileupload');
-const { AceBaseServer } = require('acebase-server');
-const server = new AceBaseServer('my_server_db', { /* server config */ });
-server.ready(() => {
-    // Server running
-});
+
 let initial_path = path.join(__dirname, "public");
 const app = express();
 app.use(express.static(initial_path));
