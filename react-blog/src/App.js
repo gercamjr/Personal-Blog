c87a46251163b89fe1e9e1e159b06d444db0249a
Gerardo Camorlinga Jr <83178346+gercamjr@users.noreply.github.com>
2022-01-21 11:39:35 -0600

    created navbar component

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import './styles/styles.css';
+import Navbar from './components/Navbar'
+
+
 
 function App() {
+
+  const blogs = {}
+
   return (
     <div className="App">
+    <Navbar />
+    <main className="main">
+      
+    </main>
+    <footer className="footer">
+
+    </footer>
       
     </div>
   );
---
+++ react-blog/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/styles.css';
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <h1>No hay Pedo Blog</h1>
+        </nav>
+    )
+}
